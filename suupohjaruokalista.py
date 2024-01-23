cd1820c0835a56e14c272602e0a1f54a66268686
JOPA9000 <157454802+JOPA9000@users.noreply.github.com>
2024-01-23 10:41:17 +0200

    Add files via upload

---
+++ suupohjaruokalista.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://sedu.fi/kampus/sedu-seinajoki-suupohjantie/#ruokalista"
+response = requests.get(url)
+soup = BeautifulSoup(response.content, 'html.parser')
+header = soup.find(lambda tag:tag.name=="h1" and "Viikon ruokalista" in tag.text)
+element = soup.find(id='ruokalista')
+text = element.text
+lines = text.split('\n')
+weekdays = ['Maanantai', 'Tiistai', 'Keskiviikko', 'Torstai', 'Perjantai']
+
+directory_path = "C:\\Users\\Janne\\Desktop\\"
+
+with open(f'{directory_path}/Ruokalista.txt', 'w') as f:# Open the file in write mode ('w')
+    for line in lines:
+        if line.strip():
+            if any(line.startswith(weekday) for weekday in weekdays):
+                f.write('\n')  # Write an empty line before the weekday
+            f.write(line + '\n')  # Write the line to the file
